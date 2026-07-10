a4b18fa01dc60ac0c666d1561b4a3f66b875bc27
Vihangi Wijesooriya <140702785+Vihangi-Wijesooriya@users.noreply.github.com>
2026-07-10 09:58:27 +0530

    Delete inventory-system directory

--- inventory-system/includes/footer.php
+++
@@ -1,9 +0,0 @@
-        </main>
-    </div>
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="<?= url('assets/js/app.js') ?>"></script>
-</body>
-</html>
-<?php take_old(); ?>
